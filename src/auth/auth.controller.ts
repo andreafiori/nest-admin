d6d96bb6c1af773de3ba7fad58a9cf20e8a1a071
Andrea Fiori <a.fiori@cheapnet.it>
2021-05-29 23:42:33 +0200

    AuthGuard, commonModule, user create and get

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,77 +1,79 @@
-import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, NotFoundException, Post, Req, Res, UseInterceptors } from '@nestjs/common';
-import {Request, Response} from 'express';
+import { BadRequestException, Body, ClassSerializerInterceptor, Controller, Get, NotFoundException, Post, Req, Res, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Request, Response } from 'express';
 import { UserService } from '../user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './model/register.dto';
 import { JwtService } from '@nestjs/jwt';
+import { AuthGuard } from './auth.guard';
 
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller()
 export class AuthController {
 
-    constructor(
-        private userService: UserService,
-        private jwtService: JwtService,
-    ) {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {
+  }
+
+  @Post('register')
+  async register(@Body() body: RegisterDto) {
+    if (body.password !== body.password_confirm) {
+      throw new BadRequestException('Passwords do not match!');
     }
 
-    @Post('register')
-    async register(@Body() body: RegisterDto) {
-        if (body.password !== body.password_confirm) {
-            throw new BadRequestException('Passwords do not match!');
-        }
+    const hashedPassword = await bcrypt.hash(body.password, 12);
 
-        const hashedPassword = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashedPassword,
+    });
+  }
 
-        return this.userService.create({
-            first_name: body.first_name,
-            last_name: body.last_name,
-            email: body.email,
-            password: hashedPassword,
-        });
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({ passthrough: true }) response: Response
+  ) {
+    const user = await this.userService.findOne({ email: email });
+
+    if (!user) {
+      throw new NotFoundException('User not foud');
     }
 
-    @Post('login')
-    async login(
-        @Body('email') email: string,
-        @Body('password') password: string,
-        @Res() response: Response
-    ) {
-        const user = await this.userService.findOne({email: email});
-
-        if (!user) {
-            throw new NotFoundException('User not foud');
-        }
-
-        if (!password) {
-            throw new BadRequestException('Password is not set');
-        }
-
-        if (!await bcrypt.compare(password, user.password)) {
-            throw new BadRequestException('Invalid credentials');
-        }
-
-        const jwt = await this.jwtService.signAsync({id: user.id});
-        response.cookie('jwt', jwt, {httpOnly: true});
-
-        return user;
+    if (!password) {
+      throw new BadRequestException('Password is not set');
     }
 
-    @Get('user')
-    async user(@Req() request: Request) {
-        const cookie = request.cookies['jwt'];
-
-        const data = await this.jwtService.verifyAsync(cookie);
-
-        return this.userService.findOne({id: data.id});
+    if (!await bcrypt.compare(password, user.password)) {
+      throw new BadRequestException('Invalid credentials');
     }
 
-    @Post('logout')
-    async logout(@Res() response: Response) {
-        response.clearCookie('jwt');
+    const jwt = await this.jwtService.signAsync({ id: user.id });
+    response.cookie('jwt', jwt, { httpOnly: true });
 
-        return {
-            message: 'Success'
-        };
-    }
+    return user;
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('user')
+  async user(@Req() request: Request) {
+    const cookie = request.cookies['jwt'];
+
+    const data = await this.jwtService.verifyAsync(cookie);
+
+    return this.userService.findOne({ id: data.id });
+  }
+
+  @Post('logout')
+  async logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('jwt');
+
+    return {
+      message: 'Success'
+    };
+  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, ClassSerializerInterceptor, UseInterceptors, UseGuards, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './models/user.entity';
+import * as bcrypt from 'bcryptjs';
+import { UserCreateDto } from './models/user.create.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
+@UseInterceptors(ClassSerializerInterceptor)
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
 
-    constructor(private userService: UserService) {
-    }
+  constructor(private userService: UserService) {
+  }
 
-    @Get()
-    async all(): Promise<User[]> {
-        return this.userService.all();
-    }
+  @Get()
+  async all(): Promise<User[]> {
+    return this.userService.all();
+  }
+
+  @Post()
+  async create(@Body() body: UserCreateDto): Promise<User> {
+    const password = await bcrypt.hash('1234', 12);
+
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: password
+    });
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: number) {
+    return this.userService.findOne(id); 
+  }
 }
